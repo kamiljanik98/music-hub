59d25d6f5863b9fd2d3c777ea57e0b6066bc201b
Kamil-Janik <yaneczq1@gmail.com>
2025-07-14 16:42:32 +0200

    Create reusable InputFile component with react-hook-form Controller

---
+++ components/forms/inputs/InputFile.tsx
@@ -0,0 +1,32 @@
+import { FC } from "react";
+import { useFormContext, Controller } from "react-hook-form";
+
+type InputFileProps = {
+  name: string;
+  accept?: string;
+};
+
+const InputFile: FC<InputFileProps> = ({ name, accept }) => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({ field: { onChange } }) => (
+        <input
+          type="file"
+          accept={accept}
+          multiple={false}
+          onChange={(e) => {
+            const files = e.target.files;
+            if (!files) return;
+            onChange(files[0])
+          }}
+        />
+      )}
+    />
+  );
+};
+
+export default InputFile;
