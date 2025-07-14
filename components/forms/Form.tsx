f0c4a4b6d6edec2a707c05038c3138c3d7bd05e1
Kamil Janik <yaneczq1@gmail.com>
2025-07-14 16:33:26 +0200

    Create form components (#1)
    * Introduce react-hook-form and create reusable Form component
    
    * Create InputText using react-hook-form Controller

---
+++ components/forms/Form.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import { FormProvider, useForm, SubmitHandler } from "react-hook-form";
+
+type FormProps = {
+  children: React.ReactNode;
+  onSubmit: SubmitHandler<any>;
+  className: string;
+};
+
+const Form: FC<FormProps> = ({ children, onSubmit }) => {
+  const methods = useForm();
+  return (
+    <FormProvider {...methods}>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
+    </FormProvider>
+  )
+}
+
+export default Form;
---
+++ components/forms/inputs/InputText.tsx
@@ -0,0 +1,28 @@
+import { FC } from "react"
+import { useFormContext, Controller } from "react-hook-form";
+
+type InputTextProps = {
+  name: string;
+  placeholder: string;
+}
+
+const InputText: FC<InputTextProps> = ({name, placeholder}) => {
+  const { control } = useFormContext();
+
+  return (
+    <Controller
+      control={control}
+      name={name}
+      render={({field}) => (
+        <input 
+          {...field}
+          type="text"
+          value={field.value}
+          placeholder={placeholder}
+        />
+      )}
+    />
+  )
+}
+
+export default InputText;
