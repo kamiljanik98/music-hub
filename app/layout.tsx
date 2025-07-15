6b1a83f36ade7ea2353a36af662cd2a4cf5c9672
Kamil-Janik <yaneczq1@gmail.com>
2025-07-15 14:17:43 +0200

    Introduce SupabaseProvider to initialize and provide Supabase client context

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import SupabaseProvider from '@/providers/SupabaseProvider';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
@@ -17,9 +18,11 @@ export default function RootLayout({
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <body className={`${inter.variable} antialiased`}>
-          {children}
-      </body>
+      <SupabaseProvider>
+        <body className={`${inter.variable} antialiased`}>
+            {children}
+        </body>
+      </SupabaseProvider>
     </html>
   );
 }
