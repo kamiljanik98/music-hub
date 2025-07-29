898d9acbec7a52f445a77b276b64addc7dade145
Kamil-Janik <yaneczq1@gmail.com>
2025-07-29 13:12:15 +0200

    Fix missing songUrl in Player component

--- components/audio/Player.tsx
+++ components/audio/Player.tsx
@@ -1,20 +1,23 @@
 "use client";
 
 import useGetSongById from "@/hooks/useGetSongById";
+import useLoadSongUrl from "@/hooks/useLoadSongUrl";
 import usePlayer from "@/hooks/usePlayer";
 import PlayerContent from "./PlayerContent";
+import { Song } from "@/types";
 
 const Player = () => {
   const player = usePlayer();
   const { song } = useGetSongById(player.activeId);
+  const songUrl = useLoadSongUrl(song as Song);
 
-  if (!song || !player.activeId) {
+  if (!song || !songUrl || !player.activeId) {
     return null;
   }
 
   return (
     <div className="fixed bottom-0 bg-black w-full py-2 h-[80px] px-4">
-      <PlayerContent song={song} songUrl={song.path} />
+      <PlayerContent key={song.path} song={song} songUrl={songUrl} />
     </div>
   );
 };
