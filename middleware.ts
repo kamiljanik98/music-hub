0bec499f94ea1f551cfc722f7a5292920f0dbad9
Kamil-Janik <yaneczq1@gmail.com>
2025-08-03 22:28:49 +0200

    fix(auth): prevent redirect loop on logout

--- middleware.ts
+++ middleware.ts
@@ -4,12 +4,18 @@ import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
   const token = request.cookies.get(`sb-${process.env.PROJECT_REF}-auth-token`)?.value;
 
-  if (!token) {
+  if (request.nextUrl.pathname === '/') {
+    return NextResponse.next();
+  }
+
+  
+  if (!token && request.nextUrl.pathname.startsWith('/dashboard')) {
     return NextResponse.redirect(new URL('/', request.url));
   }
+
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: '/dashboard/:path*',
+  matcher: ['/dashboard/:path*', '/'],
 };
