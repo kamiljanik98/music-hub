d93cdaf036110e050ae21c31ac51dcf2121aaf3b
Kamil Janik <janikkamil1998@gmail.com>
2025-08-07 10:58:25 +0200

    Refactor homepage (#13)
    * Modularize homepage as HomeView component
    
    * Add logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,27 +1,27 @@
-import UserProvider from '@/providers/UserProvider';
-import SupabaseProvider from '@/providers/SupabaseProvider';
-import type { Metadata } from 'next';
-import { Inter } from 'next/font/google';
-import './globals.css';
+import UserProvider from "@/providers/UserProvider";
+import SupabaseProvider from "@/providers/SupabaseProvider";
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import "./globals.css";
 
 const inter = Inter({
-  variable: '--font-inter',
-  subsets: ['latin'],
+  variable: "--font-inter",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-  title: 'Music Hub',
-  description: 'Place for music producers to share their work',
+  title: "Music Hub",
+  description: "Place for music producers to share their work",
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
-      <SupabaseProvider>
-        <UserProvider>
-          <body className={`${inter.variable} antialiased`}>{children}</body>
-        </UserProvider>
-      </SupabaseProvider>
+      <body className={`${inter.variable} antialiased`}>
+        <SupabaseProvider>
+          <UserProvider>{children}</UserProvider>
+        </SupabaseProvider>
+      </body>
     </html>
   );
 }
