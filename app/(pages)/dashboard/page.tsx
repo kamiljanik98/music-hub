552d591c58be8f2895539096457466fc48d61a43
Kamil-Janik <yaneczq1@gmail.com>
2025-08-07 10:45:02 +0200

    Add dashboard view

--- app/(pages)/dashboard/page.tsx
+++ app/(pages)/dashboard/page.tsx
@@ -1,11 +1,5 @@
-import Sidebar from '@/components/navigation/Sidebar';
-import View from './view';
+import { DashboardView } from "@/modules/dashboard/ui/views/dashboard-view";
 
 export default function DashboardPage() {
-  return (
-    <div className="flex flex-col md:flex-row h-screen w-full bg-neutral-950 p-2 relative overflow-hidden gap-2">
-      <Sidebar />
-      <View />
-    </div>
-  );
+  return <DashboardView />;
 }
