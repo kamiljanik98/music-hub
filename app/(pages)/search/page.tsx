cfd6e1cb5fadff64e935c672965e5fbd4095c20a
Kamil-Janik <yaneczq1@gmail.com>
2025-08-08 21:48:57 +0200

    fix(search): update SearchPage props to use flexible searchParams type

--- app/(pages)/search/page.tsx
+++ app/(pages)/search/page.tsx
@@ -3,13 +3,10 @@ import SearchContent from "@/modules/search/ui/components/search-content";
 import getSongsByTitle from "@/actions/getSongsByTitle";
 
 interface Props {
-  searchParams?: {
-    title?: string;
-  };
+  searchParams?: Record<string, string | string[] | undefined>;
 }
 const SearchPage = async ({ searchParams }: Props) => {
-  const songs = await getSongsByTitle(searchParams?.title ?? "");
-  console.log("searchParams received:", searchParams);
+  const songs = await getSongsByTitle((searchParams?.title as string) || "");
   return (
     <div className="flex h-full w-full flex-col justify-start bg-neutral-900 rounded-lg items-center gap-2">
       <SearchInput />
