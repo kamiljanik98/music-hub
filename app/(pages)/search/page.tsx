d2ae4a769e0bc60cb2ecfc267c0314818420cc92
Kamil-Janik <yaneczq1@gmail.com>
2025-08-08 21:55:12 +0200

    fix(search): await async searchParams to comply with Next.js 15 new PageProps type

--- app/(pages)/search/page.tsx
+++ app/(pages)/search/page.tsx
@@ -3,10 +3,15 @@ import SearchContent from "@/modules/search/ui/components/search-content";
 import getSongsByTitle from "@/actions/getSongsByTitle";
 
 interface Props {
-  searchParams?: Record<string, string | string[] | undefined>;
+  searchParams?: Promise<Record<string, string | string[] | undefined>>;
 }
+export const revalidate = 0;
+
 const SearchPage = async ({ searchParams }: Props) => {
-  const songs = await getSongsByTitle((searchParams?.title as string) || "");
+  const resolvedSearchParams = await searchParams;
+  const title = (resolvedSearchParams?.title as string) || "";
+
+  const songs = await getSongsByTitle(title);
   return (
     <div className="flex h-full w-full flex-col justify-start bg-neutral-900 rounded-lg items-center gap-2">
       <SearchInput />
